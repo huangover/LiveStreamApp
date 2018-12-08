0b1ac914f4dcea5887b23b5b15e60569ffa2d496
Sihang Huang <sihang@sihangs-macbook-pro.local>
2018-12-08 13:15:03 +0800

    Initial Commit

---
+++ LiveStreamApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveStreamApp
+//
+//  Created by Sihang Huang on 12/8/18.
+//  Copyright © 2018 Sihang Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveStreamApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStreamApp
+//
+//  Created by Sihang Huang on 12/8/18.
+//  Copyright © 2018 Sihang Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
