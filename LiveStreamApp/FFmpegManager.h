dd031900f6bb03ad288da3ea61b609ffd0fbd8b2
sihang <sihang@xiaoman.cn>
2018-12-09 21:32:52 +0800

    往项目中添加FFmpeg框架。正确引用头文件

---
+++ LiveStreamApp/FFmpegManager.h
@@ -0,0 +1,17 @@
+//
+//  FFmpegManager.h
+//  LiveStreamApp
+//
+//  Created by sihang huang on 2018/12/9.
+//  Copyright © 2018 Sihang Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFmpegManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LiveStreamApp/FFmpegManager.m
@@ -0,0 +1,17 @@
+//
+//  FFmpegManager.m
+//  LiveStreamApp
+//
+//  Created by sihang huang on 2018/12/9.
+//  Copyright © 2018 Sihang Huang. All rights reserved.
+//
+
+#import "FFmpegManager.h"
+#import <libavformat/avformat.h>
+#import <libswscale/swscale.h>
+#import <libswresample/swresample.h>
+#import <libavutil/pixdesc.h>
+
+@implementation FFmpegManager
+
+@end
